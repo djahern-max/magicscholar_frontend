715ba36ab42f297bb8a396558d5213d9a1d2146e
djahern-max <danielaherniv@gmail.com>
2025-09-11 12:49:10 -0400

    changed placeholder for search field

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -187,7 +187,7 @@ export default function Home() {
                     <Search className="absolute left-4 top-1/2 transform -translate-y-1/2 text-gray-400 w-5 h-5" />
                     <input
                       type="text"
-                      placeholder="Search for schools..."
+                      placeholder="Your Future is Calling..."
                       value={searchQuery}
                       onChange={(e) => setSearchQuery(e.target.value)}
                       onKeyDown={(e) => {
